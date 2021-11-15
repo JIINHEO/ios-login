716edcb0b6c3acc860016b69aa2db3b9c60c771b
JIINHEO <gjwldls123@gmail.com>
2021-11-15 15:10:48 +0900

    [#3] Feat: Email Login 기능 적용

--- ios-login/MainViewController.swift
+++ ios-login/MainViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class MainViewCotnroller: UIViewController {
+class MainViewController: UIViewController {
     
     @IBOutlet weak var welcomeLabel: UILabel!
     
@@ -23,6 +24,13 @@ class MainViewCotnroller: UIViewController {
         
         // navigationBar 숨기기
         navigationController?.navigationBar.isHidden = true
+    
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+        환영합니다.
+        \(email)님
+        """
     }
     
     @IBAction func logoutButtonTapped(_ sender: UIButton){
